a1a554de4a3dee199708b81a8a8dc45a00a26f4b
yangwei999 <348134071@qq.com>
2024-03-26 10:36:18 +0800

    remove python

--- framework-python/robotgitee/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from robotgitee import client
-from robotgitee import logutil
-from robotgitee import framework
--- framework-python/robotgitee/client/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from robotgitee.client.interface import Client
-from robotgitee.client.impl import new_client
-
-
-__all__ = ["Client", "new_client"]
